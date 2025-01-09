3c8e10ac44817980964d8a27b502efe0d86cb69f
adityagithubraj <adityaraj9843@gmail.com>
2025-01-09 18:22:17 +0530

    frontend folder added2

--- Backend/controllers/elk.js
+++ Backend/controllers/elk.js
@@ -1,7 +1,7 @@
 const { Client } = require('@elastic/elasticsearch');
 const client = new Client({ node: 'http://localhost:9200' });
 
-
+//.
 async function syncToElasticsearch(post) {
   await client.index({
     index: 'blogs',
